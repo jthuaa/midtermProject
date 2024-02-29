b09c59986cf740db815151bcb2a24662b35f0595
jthuaa <157547300+jthuaa@users.noreply.github.com>
2024-02-29 17:19:37 -0600

    slight clean up of code

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-//navbar
-function hideIconBar() {
-    var iconBar = document.getElementById("iconBar")
-    var navigation = document.getElementById("navigation")
-    iconBar.setAttribute("style", "display: none;")
-    navigation.classList.remove("hide")
-}
-
-function openIconBar() {
-    var iconBar = document.getElementById("iconBar")
-    var navigation = document.getElementById("navigation")
-    iconBar.setAttribute("style", "display: block;")
-    navigation.classList.add("hide")
-}
-
-
 // Dynamically load and insert navbar.html content
 fetch('navbar.html')
     .then(response => response.text())
