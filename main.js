858487b6bbf6b4f1aa804e2885d6220d23b94bb3
Mattie <mrm4@iastate.edu>
2024-02-28 18:43:11 -0600

    Created a footer

--- main.js
+++ main.js
@@ -20,3 +20,10 @@ fetch('navbar.html')
     .then(html => {
         document.getElementById('navbarContainer').innerHTML = html;
     });
+
+// Dynamically load and insert navbar.html content
+fetch('footer.html')
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById('footerContainer').innerHTML = html;
+    });
